bcd39e8c40f0109dee3d25ead6968ddc9c12d8fc
youta tabuchi <tabuchiyouta0717@gmail.com>
2026-09-21 22:27:43 +0900

    メニュー完成

--- Shiori/src/constant/MenuConst.java
+++ Shiori/src/constant/MenuConst.java
@@ -1,5 +1,22 @@
 package constant;
 
 public class MenuConst {
+	public static void displayMenu() {
+		System.out.println("==============================");
+		System.out.println("=======     Shiori      ======");
+		System.out.println("==============================");
+		System.out.println();
+		System.out.println("1: 登録");
+		System.out.println("2: 一覧表示");
+		System.out.println("3: 更新");
+		System.out.println("4: 削除");
+		System.out.println("0: 終了");
+		System.out.println();
+		System.out.println("番号: ");
+	}
 
+	//	debug
+	//	public static void main(String[] args) {
+	//		displayMenu();
+	//	}
 }
